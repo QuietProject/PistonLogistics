23d554047c5741fda6ef00c6d463cc66f591e321
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-06-29 19:21:13 -0300

    Progress

--- JavaScript/scriptCamioneros.js
+++ JavaScript/scriptCamioneros.js
@@ -1,6 +1,7 @@
 const body = document.getElementsByTagName('body');
 const menu = document.getElementById('menu');
 const menuDesplegable = document.getElementById('menuDesplegable');
+const tl = gsap.timeline({defautls: {duration: 0.8}});
 let i = 0;
 let y = 0;
 
@@ -8,10 +9,10 @@ menuDesplegable.style.display = 'none';
 menu.addEventListener('click', ()=>{
     menuDesplegable.style.display = '';
     if (i % 2 === 0) {
-        gsap.fromTo('.menuDesplegable', {opacity: 0}, {opacity: 1, duration: 0.8});
+        tl.fromTo('.menuDesplegable', {opacity: 0, x:30}, {opacity: 1, x: 0, duration: 0.8});
         i++;
     }else{
-        gsap.fromTo('.menuDesplegable', {opacity: 1}, {opacity: 0, duration: 0.8});
+        tl.fromTo('.menuDesplegable', {opacity: 1, x:0}, {opacity: 0, x: 30, duration: 0.8});
         i++;
     }
 })
