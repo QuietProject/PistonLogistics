2ca6d667104e38df1f1576e111740c73c02d3484
Mauriciolapi <115323300+Mauriciolapi@users.noreply.github.com>
2023-07-06 17:33:10 -0300

    Add files via upload

--- JavaScript/scriptCamioneros.js
+++ JavaScript/scriptCamioneros.js
@@ -10,9 +10,38 @@ menu.addEventListener('click', ()=>{
     menuDesplegable.style.display = '';
     if (i % 2 === 0) {
         tl.fromTo('.menuDesplegable', {opacity: 0, x:30}, {opacity: 1, x: 0, duration: 0.8});
+        gsap.fromTo('.bx-menu', {color: "#000000"}, {color: "#ff9500"});
         i++;
     }else{
         tl.fromTo('.menuDesplegable', {opacity: 1, x:0}, {opacity: 0, x: 30, duration: 0.8});
+        gsap.fromTo('.bx-menu', {color: "#ff9500"}, {color: "#000000"});
         i++;
     }
 })
+
+
+const section = document.getElementById('section');
+const divs = section.querySelectorAll('div');
+
+divs.forEach((div, index) => {
+    const newDiv = document.createElement('div');
+    newDiv.className = `div${index + 1}`;
+    const info = document.createElement('div');
+    info.textContent = 'Informacion Pedido ' + (index + 1);
+    newDiv.appendChild(info);
+    div.appendChild(newDiv);
+
+    let i = 0;
+    let a = `div${index + 1}`;
+
+    div.addEventListener('click', () => {
+        if (i % 2 === 0) {
+            gsap.fromTo(`.${a}`, {opacity: 0, xPercent:0}, {opacity: 1, xPercent: 100, duration: 0.8});
+            i++;
+        }else{
+            gsap.fromTo(`.${a}`, {opacity: 1, xPercent: 100}, {opacity: 0, xPercent: 0, duration: 0.8});
+            i++;
+        }
+    })
+});
+
