4c231d1d073ae32a280a559ec43fdb510f8b3b27
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-09-12 16:12:56 -0300

    Standard HamMenu

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -4,12 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/styleCliente.css">
     <title>Document</title>
 </head>
 
 <body>
+    <!-- Ham Menu -->
+    <div class="hamMenu" id="hamMenu">
+        <i class='bx bx-menu' id="menu"></i>
+    </div>
+    <div class="sideMenu" id="sideMenu" style="right: -15vw;">
+        <input type="button" id="BtnLogOut" value="Log Out" class="itemSideMenu">
+        <input type="button" id="BtnLogOut" value="Log Out" class="itemSideMenu">
+    </div>
     <!-- QR Scanner -->
     <div class="qrContainer">
         <img src="../source/Placeholder QR.jpeg" alt="">
@@ -18,17 +27,6 @@
         <h1>Ticket Info Placeholder</h1>
     </div>
     <input type="button" value="Confirm" class="confirmButton">
-
-    <!-- Ham Menu -->
-    </div>
-    <div class="hamMenu" id="hamMenu">
-        <div class="hamMenuLine" id="hamMenuLine"></div>
-        <div class="hamMenuLine" id="hamMenuLine"></div>
-        <div class="hamMenuLine" id="hamMenuLine"></div>
-    </div>
-    <div class="sideMenu" id="sideMenu" style="right: -15vw;">
-        <input type="button" id="BtnLogOut" value="Log Out" class="itemSideMenu">
-    </div>
 </body>
 
 </html>
