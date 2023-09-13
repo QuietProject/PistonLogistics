b04151c1cbb80c98f797f1517b420923a59bf462
QuietProject <115436355+QuietProject@users.noreply.github.com>
2023-09-13 14:18:15 -0300

    Main Brach Fix

--- public/javascript/scriptCliente.js
+++ public/javascript/scriptCliente.js
@@ -2,36 +2,10 @@ document.getElementById("hamMenu").addEventListener("click", function myFunction
     let str = document.getElementById("sideMenu").style.getPropertyValue('right');
     if (str.slice(0, 2) < 0) {
         document.getElementById("sideMenu").style.right = "0";
+        document.getElementById("menu").style.color = "orange";
     }
     else {
         document.getElementById("sideMenu").style.right = "-15vw";
+        document.getElementById("menu").style.color = "var(--baseLighter)";
     }
-})
-
-document.getElementById("BtnPaquetes").addEventListener("click", function myFunction() {
-    document.getElementById("ContainerPaquetes").style.display = "flex";
-    document.getElementById("ContainerLotes").style.display = "none";
-    document.getElementById("ContainerCrearLote").style.display = "none";
-    document.getElementById("ContainerQrScanner").style.display = "none";
-})
-
-document.getElementById("BtnLotes").addEventListener("click", function myFunction() {
-    document.getElementById("ContainerPaquetes").style.display = "none";
-    document.getElementById("ContainerLotes").style.display = "flex";
-    document.getElementById("ContainerCrearLote").style.display = "none";
-    document.getElementById("ContainerQrScanner").style.display = "none";
-})
-
-document.getElementById("BtnCrearLote").addEventListener("click", function myFunction() {
-    document.getElementById("ContainerPaquetes").style.display = "none";
-    document.getElementById("ContainerLotes").style.display = "none";
-    document.getElementById("ContainerCrearLote").style.display = "flex";
-    document.getElementById("ContainerQrScanner").style.display = "none";
-})
-
-document.getElementById("BtnQrScanner").addEventListener("click", function myFunction() {
-    document.getElementById("ContainerPaquetes").style.display = "none";
-    document.getElementById("ContainerLotes").style.display = "none";
-    document.getElementById("ContainerCrearLote").style.display = "none";
-    document.getElementById("ContainerQrScanner").style.display = "flex";
 })
--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -4,70 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/styleCliente.css">
     <title>Document</title>
 </head>
 
 <body>
-    <!-- Paquetes -->
-    <div class="container" id="ContainerPaquetes" style="display: flex;">
-        <table class="tableInfo">
-            <tr>
-                <th>ID</th>
-                <th>Peso</th>
-                <th>Destino</th>
-                <th>ID Lote</th>
-            </tr>
-        </table>
-    </div>
-    <!-- Lotes -->
-    <div class="container" id="ContainerLotes" style="display: none;">
-        <table class="tableInfo">
-            <tr>
-                <th>ID</th>
-                <th>Estado</th>
-                <th>Destino</th>
-                <th></th>
-            </tr>
-        </table>
-    </div>
-    <!-- Crear Lote -->
-    <div class="container" id="ContainerCrearLote" style="display: none;">
-        <div class="qrContainer">
-            <h1>QR Cam Placeholder</h1>
-        </div>
-        <div class="ticketInfo">
-            <h1>Ticket Info Placeholder</h1>
-        </div>
-        <input type="button" value="Confirm" class="confirmButton">
-    </div>
-    <!-- QR Scanner -->
-    <div class="container" id="ContainerQrScanner" style="display: none;">
-        <div class="qrContainer">
-            <h1>QR Cam Placeholder</h1>
-        </div>
-        <div class="ticketInfo">
-            <h1>Ticket Info Placeholder</h1>
-        </div>
-        <input type="button" value="Confirm" class="confirmButton">
-    </div>
     <!-- Ham Menu -->
-    </div>
     <div class="hamMenu" id="hamMenu">
-        <div class="hamMenuLine" id="hamMenuLine"></div>
-        <div class="hamMenuLine" id="hamMenuLine"></div>
-        <div class="hamMenuLine" id="hamMenuLine"></div>
+        <i class='bx bx-menu' id="menu"></i>
     </div>
     <div class="sideMenu" id="sideMenu" style="right: -15vw;">
-        <input type="button" id="BtnPaquetes" value="Paquetes" class="itemSideMenu">
-        <input type="button" id="BtnLotes" value="Lotes" class="itemSideMenu">
-        <input type="button" id="BtnCrearLote" value="Crear Lote" class="itemSideMenu">
-        <input type="button" id="BtnQrScanner" value="QR Scanner" class="itemSideMenu">
+        <input type="button" id="BtnLogOut" value="Log Out" class="itemSideMenu">
         <input type="button" id="BtnLogOut" value="Log Out" class="itemSideMenu">
     </div>
+    <!-- QR Scanner -->
+    <div class="qrContainer">
+        <img src="../source/Placeholder QR.jpeg" alt="">
+    </div>
+    <div class="ticketInfo">
+        <h1>Ticket Info Placeholder</h1>
+    </div>
+    <input type="button" value="Confirm" class="confirmButton">
 </body>
 
 </html>
-<script>let str = document.getElementById("sideMenu").style.getPropertyValue('right');</script>
+<script>
+    let str = document.getElementById("sideMenu").style.getPropertyValue('right');
+</script>
 <script src="../javascript/scriptCliente.js"></script>
