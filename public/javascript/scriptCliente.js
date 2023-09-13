df9e3c8cd057efe1f86c54a498af35f43381a560
Ignacio Tondo <nachotondo10@gmail.com>
2023-09-13 08:18:13 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/QuietProject/PistonLogistics""
    This reverts commit 6adb37cf0581ceec106af8b503461b6b560c6a4d.

--- public/javascript/scriptCliente.js
+++ public/javascript/scriptCliente.js
@@ -2,9 +2,11 @@ document.getElementById("hamMenu").addEventListener("click", function myFunction
     let str = document.getElementById("sideMenu").style.getPropertyValue('right');
     if (str.slice(0, 2) < 0) {
         document.getElementById("sideMenu").style.right = "0";
+        document.getElementById("menu").style.color = "orange";
     }
     else {
         document.getElementById("sideMenu").style.right = "-15vw";
+        document.getElementById("menu").style.color = "var(--baseLighter)";
     }
 })
 
