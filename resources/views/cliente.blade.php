0816fecf71c77de9e471f325dade4c95b1ce5a6a
IgTondo <nachotondo10@gmail.com>
2023-10-03 12:25:38 -0300

    se arregló el link con los estilos en cliente

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../CSS/style.css">
-    <link rel="stylesheet" href="../CSS/styleCliente.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/styleCliente.css">
     <title>Cliente</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
