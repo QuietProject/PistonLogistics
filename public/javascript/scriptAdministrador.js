28cedc44b199c78f5392dd05ca06e41b35fce98d
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-10-03 22:29:48 -0300

    FrontEnd de Vehiculos Pt.1

---
+++ public/javascript/scriptAdministrador.js
@@ -0,0 +1,88 @@
+// Add buttons Show add screen
+
+document.getElementById("addTruck").addEventListener("click", function myFunction() {
+    document.getElementById("addBackdrop").style.display = "flex";
+    document.getElementById("addTruckInterface").style.display = "flex";
+})
+
+// Add buttons Show add screen
+
+document.getElementById("closeButtonTrucks").addEventListener("click", function myFunction() {
+    document.getElementById("addBackdrop").style.display = "none";
+    document.getElementById("addTruckInterface").style.display = "none";
+})
+
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tableTrucks");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+function sortTableAlternate(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tableCamionetas");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
--- resources/views/vehiculos/index.blade.php
+++ resources/views/vehiculos/index.blade.php
@@ -1,69 +1,146 @@
-<x-layout titulo='Vehiculos'>
-    @include('vehiculos.create')
-    <h2>Camiones</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>matricula</th>
-                <th>Volumen maximo</th>
-                <th>Peso maximo</th>
-                <th>Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($camiones as $camion)
-                <tr>
-                    <td><a href="{{ route('vehiculos.show', $camion->matricula) }}"> {{ $camion->matricula }}</a>
-                    </td>
-                    <td>{{ $camion->peso_max }}kg</td>
-                    <td>{{ $camion->vol_max }}m3</td>
-                    <td>
-                        @if ($camion->baja)
-                            de baja
-                        @else
-                            @if ($camion->es_operativo)
-                                operativo
-                            @else
-                                fuera de servicio
-                            @endif
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<!DOCTYPE html>
+<html lang="en">
 
-    <h2>Camionetas</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>matricula</th>
-                <th>Volumen maximo</th>
-                <th>Peso maximo</th>
-                <th>Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($camionetas as $camioneta)
-                <tr>
-                    <td><a href="{{ route('vehiculos.show', $camioneta->matricula) }}"> {{ $camioneta->matricula }}</a>
-                    </td>
-                    <td>{{ $camioneta->peso_max }}kg</td>
-                    <td>{{ $camioneta->vol_max }}m3</td>
-                    <td>
-                        @if ($camioneta->baja)
-                            de baja
-                        @else
-                            @if ($camioneta->es_operativo)
-                                operativo
-                            @else
-                                fuera de servicio
-                            @endif
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="../css/style.css ">
+    <link rel="stylesheet" href="../css/styleAdministrador.css">
+    <title>Piston Logistics</title>
+</head>
 
-</x-layout>
+<body>
+    <div class="navDiv">
+        <a href="{{ route('camioneros.index') }}" class="button inactive" id="btnUsers"></a>
+        <a href="{{ route('vehiculos.index') }}" class="button active" id="btnTrucks"></a>
+        <a href="" class="button inactive" id="btnRutes"></a>
+        <a href="" class="button inactive" id="btnWarehouses"></a>
+        <a href="" class="button inactive" id="btnProducts"></a>
+        <a href="{{ route('clientes.index') }}" class="button inactive" id="btnClients"></a>
+    </div>
+    <!-- Backdrop Blur -->
+    <div class="addBackdrop disabled" id="addBackdrop">
+    </div>
+    <!-- Trucks Screen -->
+    <div class="display" id="displayTrucks">
+        <!-- Title -->
+        <h1 class="titleText">Vehicles</h1>
+        <!-- Add Button -->
+        <input type="button" value="Add" class="addButton" id="addTruck">
+        <!-- SearchBar -->
+        <input type="text" id="" class="filterText" placeholder="Search">
+        <!-- Tables Container -->
+        <div class="tableContainer">
+            <!-- Truck Table -->
+            <table class="tableView" id="tableTrucks">
+                <thead>
+                    <tr>
+                        <th style="width: 7.5%;" onclick="sortTable(0)">ID</th>
+                        <th style="width: 30%;" onclick="sortTable(1)">Name</th>
+                        <th style="width: 30%;" onclick="sortTable(2)">Surname</th>
+                        <th style="width: 20%;" onclick="sortTable(3)">Rol</th>
+                        <th style="width: 12.5%;" onclick="sortTable(4)"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($camiones as $camion)
+                        <tr>
+                            <td><a href="{{ route('vehiculos.show', $camion->matricula) }}">
+                                    {{ $camion->matricula }}</a>
+                            </td>
+                            <td>{{ $camion->peso_max }}kg</td>
+                            <td>{{ $camion->vol_max }}m3</td>
+                            <td>
+                                @if ($camion->baja)
+                                    De Baja
+                                @else
+                                    @if ($camion->es_operativo)
+                                        Operativo
+                                    @else
+                                        Fuera De Servicio
+                                    @endif
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="tableContainer" style="left: 50vw; top: -45vh;">
+            <table class="tableView" id="tableCamionetas">
+                <thead>
+                    <tr>
+                        <th style="width: 7.5%;" onclick="sortTableAlternate(0)">ID</th>
+                        <th style="width: 30%;" onclick="sortTableAlternate(1)">Name</th>
+                        <th style="width: 30%;" onclick="sortTableAlternate(2)">Surname</th>
+                        <th style="width: 20%;" onclick="sortTableAlternate(3)">Rol</th>
+                        <th style="width: 12.5%;" onclick="sortTableAlternate(4)"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($camionetas as $camioneta)
+                        <tr>
+                            <td><a style="linkVehicle" href="{{ route('vehiculos.show', $camioneta->matricula) }}">
+                                    {{ $camioneta->matricula }}</a>
+                            </td>
+                            <td>{{ $camioneta->peso_max }}kg</td>
+                            <td>{{ $camioneta->vol_max }}m3</td>
+                            <td>
+                                @if ($camioneta->baja)
+                                    De Baja
+                                @else
+                                    @if ($camioneta->es_operativo)
+                                        Operativo
+                                    @else
+                                        Fuera De Servicio
+                                    @endif
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- Add Trucks -->
+        <div class="addInterface" id="addTruckInterface" style="display: none">
+            <!-- Close Button -->
+            <div class="cornerButton">
+                <div class="closeButton" id="closeButtonTrucks">
+                    <div class="xLine" style="rotate: 45deg;"></div>
+                    <div class="xLine" style="rotate: -45deg;"></div>
+                </div>
+            </div>
+            <!-- Add Vehicle -->
+            <div class="addForm">
+                @if ($errors->any())
+                    abre el formulario
+                @endif
+                <h2>Ingresar Vehiculo</h2>
+                <form action="{{ route('vehiculos.store') }}" method="POST">
+                    @csrf
+                    <div>
+                        <label for="tipo">Tipo</label>
+                        <select name="tipo" id="tipo">
+                            <option value="camion">Camion</option>
+                            <option value="camioneta">Camioneta</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="matricula">Matricula</label>
+                        <input type="text" name="matricula" id="matricula" maxlength="7" minlength="7" required
+                            value="{{ old('matricula') }}">
+                        @error('matricula')
+                            <span style="color: red">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    @include('vehiculos.form-fields')
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<script src="../javascript/scriptAdministrador.js"></script>
