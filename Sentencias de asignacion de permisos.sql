155ce5392122bdf063701939efbb81f9116f4cb4
lucas535f1 <lucasmarsigliaf@gmail.com>
2023-10-07 01:37:51 -0300

    Se ajustaron los permisos de los usuarios sobre los procedimientos y las tablas

--- Sentencias de asignacion de permisos.sql
+++ Sentencias de asignacion de permisos.sql
@@ -32,7 +32,7 @@ GRANT SELECT ON surno.TRAE TO 'almacen'@'%';
 GRANT SELECT ON surno.PAQUETES_ALMACENES TO 'almacen'@'%';
 
 GRANT INSERT(ID_almacen, ID_pickup, direccion , mail) ON surno.PAQUETES TO 'almacen'@'%';
-GRANT UPDATE(peso, volumen, fecha_entregado, cedula) ON surno.PAQUETES TO 'almacen'@'%';
+GRANT UPDATE(peso, volumen, cedula) ON surno.PAQUETES TO 'almacen'@'%';
 
 GRANT INSERT (ID_almacen, ID_troncal, tipo) ON surno.LOTES TO 'almacen'@'%';
 GRANT UPDATE (fecha_pronto,fecha_cerrado) ON surno.LOTES TO 'almacen'@'%';
@@ -46,11 +46,14 @@ GRANT INSERT (ID_lote, matricula) ON surno.LLEVA TO 'almacen'@'%';
 GRANT UPDATE (fecha_descarga) ON surno.LLEVA TO 'almacen'@'%';
 
 GRANT INSERT (ID_paquete, matricula) ON surno.REPARTE TO 'almacen'@'%';
-GRANT UPDATE (fecha_descarga) ON surno.REPARTE TO 'almacen'@'%';
 
 GRANT INSERT (ID_paquete, matricula) ON surno.TRAE TO 'almacen'@'%';
 
 GRANT EXECUTE ON PROCEDURE surno.descargar_trae TO 'almacen'@'%';
+
+GRANT EXECUTE ON PROCEDURE surno.descargar_trae TO 'almacen'@'%';
+GRANT EXECUTE ON PROCEDURE surno.descargar_reparte TO 'almacen'@'%';
+GRANT EXECUTE ON PROCEDURE surno.entregar_paquete_pickup TO 'almacen'@'%';
 
 -- CAMIONERO
 
@@ -76,20 +79,22 @@ GRANT SELECT ON surno.PAQUETES_ALMACENES TO 'camionero'@'%';
 
 GRANT INSERT (CI, matricula) ON surno.CONDUCEN TO 'camionero'@'%';
 GRANT UPDATE (hasta) ON surno.CONDUCEN TO 'camionero'@'%';
-GRANT UPDATE (fecha_entregado, cedula) ON surno.PAQUETES TO 'camionero'@'%';
+GRANT UPDATE (cedula) ON surno.PAQUETES TO 'camionero'@'%';
+
+GRANT EXECUTE ON PROCEDURE surno.entregar_paquete TO 'camionero'@'%';
 -- BACKOFFICE
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON surno.USERS TO 'backoffice'@'%';
 GRANT SELECT, INSERT, UPDATE ON surno.CAMIONEROS TO 'backoffice'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON surno.VEHICULOS TO 'backoffice'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON surno.CAMIONES TO 'backoffice'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON surno.CAMIONETAS TO 'backoffice'@'%';
+GRANT SELECT, DELETE ON surno.CAMIONES TO 'backoffice'@'%';
+GRANT SELECT, DELETE ON surno.CAMIONETAS TO 'backoffice'@'%';
 GRANT SELECT, INSERT(CI, matricula), UPDATE(hasta) ON surno.CONDUCEN TO 'backoffice'@'%';
 GRANT SELECT, INSERT(nombre), UPDATE(nombre,baja) ON surno.TRONCALES TO 'backoffice'@'%';
 GRANT SELECT, INSERT(RUT, NOMBRE), UPDATE ON surno.CLIENTES TO 'backoffice'@'%';
 GRANT SELECT, INSERT(nombre, direccion), UPDATE(nombre, direccion, baja) ON surno.ALMACENES TO 'backoffice'@'%';
-GRANT SELECT, INSERT, DELETE ON surno.ALMACENES_PROPIOS TO 'backoffice'@'%';
-GRANT SELECT, INSERT, DELETE ON surno.ALMACENES_CLIENTES TO 'backoffice'@'%';
+GRANT SELECT, DELETE ON surno.ALMACENES_PROPIOS TO 'backoffice'@'%';
+GRANT SELECT, DELETE ON surno.ALMACENES_CLIENTES TO 'backoffice'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON surno.ORDENES TO 'backoffice'@'%';
 GRANT SELECT, UPDATE (ID_almacen, ID_pickup, direccion, peso, volumen, mail), DELETE ON surno.PAQUETES TO 'backoffice'@'%';
 GRANT SELECT ON surno.LOTES TO 'backoffice'@'%';
@@ -97,7 +102,7 @@ GRANT SELECT, DELETE ON surno.PAQUETES_LOTES TO  'backoffice'@'%';
 GRANT SELECT ON surno.DESTINO_LOTE TO 'backoffice'@'%';
 GRANT SELECT, INSERT(ID_lote, matricula), UPDATE(fecha_descarga) ON surno.LLEVA TO 'backoffice'@'%';
 GRANT SELECT, INSERT(ID_paquete, matricula) ON surno.TRAE TO 'backoffice'@'%';
-GRANT SELECT, INSERT(ID_paquete, matricula), UPDATE(fecha_descarga) ON surno.REPARTE TO 'backoffice'@'%';
+GRANT SELECT, INSERT(ID_paquete, matricula) ON surno.REPARTE TO 'backoffice'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON surno.PAQUETES_ALMACENES TO 'backoffice'@'%';
 
 GRANT EXECUTE ON PROCEDURE surno.almacen_cliente TO 'backoffice'@'%';
@@ -105,7 +110,12 @@ GRANT EXECUTE ON PROCEDURE surno.almacen_propio TO 'backoffice'@'%';
 GRANT EXECUTE ON PROCEDURE surno.camioneta TO 'backoffice'@'%';
 GRANT EXECUTE ON PROCEDURE surno.camion TO 'backoffice'@'%';
 GRANT EXECUTE ON PROCEDURE surno.descargar_trae TO 'backoffice'@'%';
+GRANT EXECUTE ON PROCEDURE surno.descargar_reparte TO 'backoffice'@'%';
+GRANT EXECUTE ON PROCEDURE surno.entregar_paquete TO 'backoffice'@'%';
+GRANT EXECUTE ON PROCEDURE surno.entregar_paquete_pickup TO 'backoffice'@'%';
 -- SHOW GRANTS FOR 'backoffice'@'%';
 -- SHOW GRANTS FOR 'almacen'@'%';
 -- SHOW GRANTS FOR 'autentificacion'@'%';
 -- SHOW GRANTS FOR 'camionero'@'%';
+-- show procedure status;
+-- REPAIR TABLE mysql.procs_priv;
