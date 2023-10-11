82a772afa1a19874d44cce3a9dc25f9c3b6bf8fa
Mauriciolapi <mauriciolapi.14@gmail.com>
2023-10-11 04:18:49 -0300

    Arreglos de homepage

--- public/javascript/scriptIndex.js
+++ public/javascript/scriptIndex.js
@@ -33,31 +33,41 @@ setTimeout(() => {
 }, 3000);
 
 const textoAbout = document.querySelector(".textoAbout");
-const textoPreguntas1 = document.querySelector(".textoPreguntas1");
-const textoPreguntas2 = document.querySelector(".textoPreguntas2");
+const textoPreguntas = document.querySelector(".textoPreguntas");
 const sobreNosotros = document.querySelector(".sobreNosotros");
 const preguntas = document.querySelector(".preguntas");
 const animateElements1 = textoAbout.querySelectorAll(".animar");
-const animateElements2 = textoPreguntas1.querySelectorAll(".animar");
-const animateElements3 = textoPreguntas2.querySelectorAll(".animar");
+const animateElements2 = textoPreguntas.querySelectorAll(".animar");
 
 const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            sobreNosotros.style.animation = `fade 1s ease 0.2s forwards`;
-            preguntas.style.animation = `fade 1s ease 0.8s forwards`;
-            animateElements1.forEach((element, index) => {
-                element.style.animation = `fade 1s ease ${index * 0.2 + 0.4}s forwards`;
-            });
-            animateElements2.forEach((element, index) => {
-              element.style.animation = `fade 1s ease ${index * 0.2 + 0.8}s forwards`;
-            });
-            animateElements3.forEach((element, index) => {
-              element.style.animation = `fade 1s ease ${index * 0.2 + 0.8}s forwards`;
-          });
-            observer.disconnect();
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      sobreNosotros.style.animation = `fade 1s ease 0.2s forwards`;
+      preguntas.style.animation = `fade 1s ease 0.8s forwards`;
+      animateElements1.forEach((element, index) => {
+        element.style.animation = `fade 1s ease ${index * 0.2 + 0.4}s forwards`;
+      });
+      animateElements2.forEach((element, index) => {
+        element.style.animation = `fade 1s ease ${index * 0.2 + 0.8}s forwards`;
+      });
+
+      observer.disconnect();
+    }
+  });
 });
 
 observer.observe(textoAbout);
+
+
+document.getElementById("aboutUsLink").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const offset = 100;
+  const target = document.querySelector(this.getAttribute("href"));
+  const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth"
+  });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,15 +20,16 @@
                 <h1>Quick Carry</h1>
             </div>
             <div class="">
-                <i class='bx bx-menu' id="menu"></i> 
+                <i class='bx bx-menu' id="menu"></i>
             </div>
         </header>
         <div class="sidebar" id="sidebar">
             <div>
-                <i class='bx bx-x' id="closeMenu"></i> 
+                <i class='bx bx-x' id="closeMenu"></i>
             </div>
-            <a href="#home">Home</a>
-            <a href="#aboutUs">Sobre Nosotros</a>
+            <a href="#home" id="homeLink">Home</a>
+            <a href="#aboutUs" id="aboutUsLink">Sobre Nosotros</a>
+            <a href="#preguntas" id="preguntasLink">Preguntas Frecuentes</a>
         </div>
         <section id="home" class="home">
             <div class="menuRastreo">
@@ -48,43 +49,49 @@
         <section id="aboutUs" class="aboutUs">
             <h3 class="animar sobreNosotros">Sobre Nosotros</h3>
             <div class="textoAbout">
-                <p class="animar">En Quick Carry, nos dedicamos al transporte de paquetería en distribución nacional. Somos una empresa
+                <p class="animar">En Quick Carry, nos dedicamos al transporte de paquetería en distribución nacional.
+                    Somos una empresa
                     líder en el sector logístico, con una amplia red de plataformas estratégicamente ubicadas en la
-                    proximidad de las principales ciudades y centros poblados en la casi totalidad de los departamentos.</p>
+                    proximidad de las principales ciudades y centros poblados en la casi totalidad de los departamentos.
+                </p>
                 <br>
-                <p class="animar">Contamos con un equipo altamente capacitado de profesionales en logística y transporte que se dedican a
+                <p class="animar">Contamos con un equipo altamente capacitado de profesionales en logística y transporte
+                    que se dedican a
                     garantizar la entrega oportuna y segura de cada paquete. Nuestros conductores, administradores y
                     personal de atención al cliente están comprometidos con la excelencia en el servicio, brindando una
                     experiencia excepcional a nuestros clientes en cada etapa del proceso de transporte.</p>
             </div>
-            <h3 class="animar preguntas">Preguntas Frecuentes</h3>
-            <div class="textoPreguntas1">
+            <h3 class="animar preguntas" id="preguntas">Preguntas Frecuentes</h3>
+            <div class="textoPreguntas">
                 <div class="animar">
                     <h3>¿Cuál es el alcance geográfico de sus servicios de logística y transporte?</h3>
-                    <p>Ofrecemos servicios de logística y transporte en todo Uruguay, cubriendo todo el país de manera eficiente.</p>
+                    <p>Ofrecemos servicios de logística y transporte en todo Uruguay, cubriendo todo el país de manera
+                        eficiente.</p>
                 </div>
                 <div class="animar">
                     <h3>¿Cuáles son sus tiempos de entrega estándar?</h3>
-                    <p>Nuestros tiempos de entrega están diseñados para ser rápidos, de acuerdo a nuestra filosofía de servicio.</p>
+                    <p>Nuestros tiempos de entrega están diseñados para ser rápidos, de acuerdo a nuestra filosofía de
+                        servicio.</p>
                 </div>
                 <div class="animar">
                     <h3>¿Ofrecen servicios de seguimiento en tiempo real para los envíos?</h3>
-                    <p>Ofrecemos servicios de seguimiento en tiempo real para que los clientes puedan rastrear sus envíos desde el punto de recogida hasta su destino final.</p>
+                    <p>Ofrecemos servicios de seguimiento en tiempo real para que los clientes puedan rastrear sus
+                        envíos desde el punto de recogida hasta su destino final.</p>
                 </div>
-            </div>
-            <div class="textoPreguntas2">
                 <div class="animar">
-                        <h3>¿Cómo manejan situaciones de pérdida o daño de mercancía durante el transporte?</h3>
-                        <p>En caso de pérdida o daño de mercancía, tenemos políticas claras de indemnización y trabajamos para resolver cualquier problema de manera rápida y justa.</p>
-                    </div>
-                    <div class="animar">
-                        <h3>¿Cuáles son sus políticas de facturación y tarifas?</h3>
-                        <p>Nuestras políticas de facturación son transparentes y nuestras tarifas son competitivas, adaptadas a las necesidades de cada cliente.</p>
-                    </div>
-                    <div class="animar">
-                        <h3>¿Qué medidas de seguridad implementan para proteger los envíos?</h3>
-                        <p>Implementamos estrictas medidas de seguridad para proteger los envíos, desde el manejo seguro de la mercancía hasta la seguridad en nuestras instalaciones y durante el transporte.</p>
-                    </div>
+                    <h3>¿Cómo manejan situaciones de pérdida o daño de mercancía durante el transporte?</h3>
+                    <p>En caso de pérdida o daño de mercancía, tenemos políticas claras de indemnización y trabajamos
+                        para resolver cualquier problema de manera rápida y justa.</p>
+                </div>
+                <div class="animar">
+                    <h3>¿Cuáles son sus políticas de facturación y tarifas?</h3>
+                    <p>Nuestras políticas de facturación son transparentes y nuestras tarifas son competitivas,
+                        adaptadas a las necesidades de cada cliente.</p>
+                </div>
+                <div class="animar">
+                    <h3>¿Qué medidas de seguridad implementan para proteger los envíos?</h3>
+                    <p>Implementamos estrictas medidas de seguridad para proteger los envíos, desde el manejo seguro de
+                        la mercancía hasta la seguridad en nuestras instalaciones y durante el transporte.</p>
                 </div>
             </div>
         </section>
