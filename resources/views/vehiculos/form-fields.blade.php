4095b6da53634aca511133cb014ae2926900a835
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-10-12 17:32:20 -0300

    finializando vista de vehiculos

--- resources/views/vehiculos/form-fields.blade.php
+++ resources/views/vehiculos/form-fields.blade.php
@@ -1,14 +1,16 @@
-<div>
+<div class="inputBox">
     <label for="peso_max">Peso Maximo</label>
-    <input type="number" name="peso_max" id="peso_max" required value="{{ old('peso_max',$vehiculo->peso_max) }}">
+    <input type="number" name="peso_max" id="peso_max" required
+    value="{{ old('peso_max',$vehiculo->peso_max) }}" autocomplete="off">
     <span>kg</span>
     @error('peso_max')
         <span style="color: red">{{ $message }}</span>
     @enderror
 </div>
-<div>
+<div class="inputBox">
     <label for="vol_max">Volumen Maximo</label>
-    <input type="number" name="vol_max" id="vol_max" step="0.1" required value="{{ old('vol_max',$vehiculo->vol_max) }}">
+    <input type="number" name="vol_max" id="vol_max" step="0.1" required
+    value="{{ old('vol_max',$vehiculo->vol_max) }}" autocomplete="off">
     <span>m3</span>
     @error('vol_max')
         <span style="color: red">{{ $message }}</span>
