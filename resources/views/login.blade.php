b61bc2f7855fe8b2225c3cc8793cb45be097e4b0
Mauriciolapi <mauriciolapi.14@gmail.com>
2023-10-20 14:42:37 -0300

    Nuevo Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,18 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/styleLogin.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Piston Logistics</title>
 </head>
 
 <body>
     <form class="logInContainer" method="POST" action=" {{ route('login') }} ">
         @csrf
-        
-        <img src="../Source/logoNegro.svg" alt="Icon" class="iconImage">
-        <h1 class="title">Piston Logistics</h1>
-        <input type="text" name="user" required placeholder="User" style="margin-top: 15%;">
-        <input type="password" name="password" required placeholder="Password" style="margin-top: 5%;">
-        <input type="submit" value="Log In" style="margin-top: 30%;" id="buttonLogIn">
+
+        <div>
+            <i class='bx bxs-truck'></i>
+            <h1>Log In</h1>
+            <i class='bx bxs-truck'></i>
+        </div>
+
+        <div>
+            <input type="text" name="user" required placeholder="User">
+            <input type="password" name="password" required placeholder="Password">
+        </div>
+        <div>
+            <input type="submit" value="Log In" id="buttonLogIn">
+        </div>
     </form>
 </body>
 
