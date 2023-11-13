61b77009b8741a95df6db69ab22e9008d497eeea
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-11-13 01:40:18 -0300

    Backoffice sin login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,5 @@
 <x-layout menu="8" titulo="Inicio">
-    <h2>{{ __("Inicio") }}</h2>
+    <div class="display">
+        <h2 style="font-weight: 500; font-size: 5vh;position: relative; top: 45vh; text-align: center; color: white; text-shadow: 0px 0px 5px rgba(255,255,255,0.5);">{{ __('Bienvenido') }} {{auth()->user()->user}}</h2>
+    </div>
 </x-layout>
