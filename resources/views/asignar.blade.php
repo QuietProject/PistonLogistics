f50324ced7f5857cc4720cec0666ed9a1326a320
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-11-12 21:43:20 -0300

    asignar menu

--- resources/views/asignar.blade.php
+++ resources/views/asignar.blade.php
@@ -1,11 +1,15 @@
 <x-layout titulo='Inicio' menu='7' import1="../css/styleAsignarMenu.css">>
     <div class="display">
-        <h2>Asignar</h2>
-        <a href="{{ route('lleva.index') }}" class="submitBtn">Asignar Lleva</a>
-        <a href="{{ route('reparte.index') }}" class="submitBtn">Asignar Reparte</a>
-        <a href="{{ route('trae.index') }}" class="submitBtn">Asignar Trae</a>
-        <a href="{{ route('lleva.desasignar') }}" class="submitBtn">Desasignar Lleva</a>
-        <a href="{{ route('reparte.desasignar') }}" class="submitBtn">Desasignar Reparte</a>
-        <a href="{{ route('trae.desasignar') }}" class="submitBtn">Desasignar Trae</a>
+        <h2 class="titleText">Asignar</h2>
+        <div class="btnContainer">
+            <a href="{{ route('lleva.index') }}" class="submitBtn">Asignar Lleva</a>
+            <a href="{{ route('reparte.index') }}" class="submitBtn">Asignar Reparte</a>
+            <a href="{{ route('trae.index') }}" class="submitBtn">Asignar Trae</a>
+        </div>
+        <div class="btnContainer">
+            <a href="{{ route('lleva.desasignar') }}" class="submitBtn">Desasignar Lleva</a>
+            <a href="{{ route('reparte.desasignar') }}" class="submitBtn">Desasignar Reparte</a>
+            <a href="{{ route('trae.desasignar') }}" class="submitBtn">Desasignar Trae</a>
+        </div>
     </div>
 </x-layout>
