7a0fa768258afb19cc113b8258de1ca1ba4e810e
QuietProject <QuietProjectTwitchTVCFTVGY@gmail.com>
2023-11-14 19:34:21 -0300

    fix almacenes

--- resources/views/password/request.blade.php
+++ resources/views/password/request.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/stylePswForg.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>{{ __('Contraseña olvidada') }} - Piston Logistics</title>
 </head>
 
